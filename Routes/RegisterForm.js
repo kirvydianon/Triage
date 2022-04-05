dc2e85005f6c3e53254731eee7799cc4eb840f81
kirvydianon <dianon.kirvy@gmail.com>
2022-04-05 13:29:52 +0800

    first commit

--- Routes/RegisterForm.js
+++ Routes/RegisterForm.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { RegisterForm } = require("../models");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { validateToken } = require("../middleware/Auth");
 const { sign } = require("jsonwebtoken");
 
